8d3743e20eb541078d613c1784f0bda7c33898d1
Rakesh-Eskon <109338170+Rakesh-Eskon@users.noreply.github.com>
2024-01-13 19:20:15 +0530

    Update package.json and build

---
+++ src/component/Footer.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+
+function Footer() {
+  return (
+    <div>
+        {/* ======= Footer ======= */}
+      <footer id="footer">
+        <div className="container">
+          <div className="copyright">
+            &copy; Copyright
+            <strong>
+              <span>iPortfolio</span>
+            </strong>
+          </div>
+          <div className="credits">
+            Designed by
+            <a href="index.html">Rk</a>
+          </div>
+        </div>
+      </footer>
+      {/* End  Footer */}
+    </div>
+  )
+}
+
+export default Footer
